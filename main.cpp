341a1363622dd124cbcbf846c603d5a6e07b9ec3
Enzo <enzo@etbox.org>
2015-09-09 01:27:43 +0800

    add timer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 using namespace std;
+#include "timer.h"
+#include <string>
+
+int quickSort(int s,int n)
+{
+// fill in
+}
+
+int mergeSort(int s,int n)
+{
+// fill in
+}
+
+int radixSort(int s,int n)
+{
+// fill in
+}
 
 int main()
 {
-    cout<<"Hello";
+    int n=10000;
+    int S[n];
+
+    cout<<"Original: \n";
+
+    for(int i=0;i<n;i++)
+    {
+        TICK();
+        //_sleep(2000);
+        S[i]=rand();
+        cout<<S[i]<<" ";
+        TOCK();
+
+    }
+    cout<<"Time taken= "<<DURATION();
 }
