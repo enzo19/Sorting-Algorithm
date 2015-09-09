8217bb2a7e721e5af933dc12fff890ea5bde3b35
Enzo <enzo@etbox.org>
2015-09-09 20:53:43 +0800

    merge-sort v1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
-using namespace std;
 #include "timer.h"
 #include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 int quickSort(int s,int n)
 {
 // fill in
 }
 
-int mergeSort(int s,int n)
+void merge(int* A, int* B, int l, int m, int r)
 {
-// fill in
+int i, j;
+for (i = m + 1; i > l; i--) B[i - 1] = A[i - 1];
+    for (j = m; j < r; j++) B[r + m - j] = A[j + 1];
+        for (int k = l; k <= r; k++)
+            if (B[j] < B[i])
+                A[k] = B[j--];
+            else
+                A[k] = B[i++];
+}
+
+void mergesort(int* A, int* B, int l, int r,int n)
+{
+    if(l<r){
+        int m=(l+r)/2;
+        mergesort(A,B,l,m,n);
+        mergesort(A,B,m+1,r,n);
+        merge(A,B,l,m,r);
+
+        cout<<"p="<<l<<", r="<<r<<", array=";
+        for(int i=0;i<n;i++)
+        {
+            cout<<A[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int startMergeSort(int* A, int l, int r,int n)
+{
+    TICK();
+    int* B = new int[r - l + 1];
+    mergesort(A,B,l,r,n);
+    delete [] B;
+    TOCK();
+    cout<<"Time taken= "<<DURATION();
 }
 
 int radixSort(int s,int n)
@@ -20,25 +56,42 @@ int radixSort(int s,int n)
 
 int main()
 {
-
     cout<<"######################################################"<<endl;
     cout<<"# Comparison of Quick-sort,Radix-sort and Merge-sort #"<<endl;
     cout<<"######################################################"<<endl<<endl;
-    cout<<"Please input the size of array";
+    cout<<"Please input the size of array: ";
     int n;
     cin>>n;
     int S[n];
+    int mergeArray[n];
+    int quickArray[n];
+    int radixArray[n];
 
-    cout<<"Original: \n";
-
+    cout<<"Before: ";
+    //Initialize random array
+    srand(time(NULL));
     for(int i=0;i<n;i++)
     {
-        TICK();
-        //_sleep(2000);
-        S[i]=rand();
+        S[i]=rand()%9999+0; //limit to 4 digit max? to be modified,maybe
         cout<<S[i]<<" ";
-        TOCK();
-
     }
-    cout<<"Time taken= "<<DURATION();
+    cout<<endl;
+    //copy array for reuse
+    for(int i=0;i<n;i++)
+    {
+        mergeArray[i]=S[i];
+        quickArray[i]=S[i];
+        radixArray[i]=S[i];
+    }
+
+    //Quick-sort
+
+
+    //Radix-sort
+
+
+    //Merge-sort
+    startMergeSort(mergeArray,0,n-1,n);
+
+    return 0;
 }
