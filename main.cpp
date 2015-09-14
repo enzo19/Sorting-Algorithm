fbc7a7cb7efc8c30c51a8ac06c28c12aab778268
Rey Ng <reyalmightyz@gmail.com>
2015-09-14 12:56:02 +0800

    sohai

--- main.cpp
+++ main.cpp
@@ -9,100 +9,73 @@
 
 using namespace std;
 
-int partition(int* A,int p, int r)
+int partition(int* A,int p, int r, bool steps, int n)
 {
-    int i = p-1;
+    int i = p-1;    //set i to the part beside smallest index, p
     int j;
-    for(j = p; j<r ;j++)
+    for(j = p; j<r ;j++)   //loop each index until r
     {
-        if (A[j] <= A[r]){
+        if (A[j] <= A[r]){ //Compare index j with pivot, last index is chosen as pivot
             i++;
-            swap(A[i], A[j]);
+            swap(A[i], A[j]); //If j is larger swap with index i
         }
     }
 
-    swap(A[i + 1], A[r]);
-return i + 1;
+    swap(A[i + 1], A[r]);   //swap the pivot to i+1, which is right side of the partition wall
+    if(steps){
+        cout<<"array=";
+        for(int i=0;i<n;i++)  //display each steps of swap in partitioning, left smaller than pivot, right larger than pivot
+        {
+            cout<<A[i]<<" ";
+        }
+        cout<<endl;
+    }
+return i + 1; //return index to quick sort function to perform ascending order sort
 }
 
-void quickSort(int* A, int p, int r, bool steps)
-{
-    int pivot;
-    if (p < r)
-    {
-        pivot = partition(A, p, r);
-        quickSort(A, p, pivot -1,steps);
-        quickSort(A, pivot+1, r,steps);
 
+void quickSort(int* A, int p, int r, bool steps, int n)
+{
+    //Recursion
+    int pivot;
+    if (p < r)  //when p is smaller than r
+    {
+        pivot = partition(A, p, r, steps, n); //state pivot in partition function
+        quickSort(A, p, pivot -1,steps, n); //recursive function call for index left side of pivot
+        quickSort(A, pivot+1, r,steps, n);  //recursive function call for index right side of pivot
         if(steps){
         cout<<"array=";
-        for(int i=0;i<r+1;i++)
+        for(int i=0;i<n;i++)    //display each steps of swap in quick sort in ascending order, left smaller than pivot, right larger than pivot
         {
             cout<<A[i]<<" ";
         }
         cout<<endl;
         }
+
+
     }
 }
 
-/*int partition2(int* A,int p, int r)
-{
-    int i = p-1;
-    int j;
-    for(j = p; j>r ;j++)
-    {
-        if (A[j] <= A[r]){
-            i++;
-            swap(A[i], A[j]);
-        }
-    }
 
-    swap(A[i + 1], A[r]);
-return i + 1;
-}
-
-void quickSort2(int* A, int p, int r)
-{
-    int pivot;
-    if (p < r)
-    {
-        pivot = partition2(A, p, r);
-        quickSort(A, p, pivot -1);
-        quickSort(A, pivot+1, r);
-    }
-}
-*/
 //############################################################################
-
-
-void merge(int* A, int* B, int l, int m, int r)
-{
-    int i, j;
-    for (i = m + 1; i > l; i--)
-
-        B[i - 1] = A[i - 1];
-
-    for (j = m; j < r; j++)
-
-        B[r + m - j] = A[j + 1];
-
-    for (int k = l; k <= r; k++)
-        if (B[j] < B[i])
-
-            A[k] = B[j--];
-
-        else
-
-            A[k] = B[i++];
-}
 
 void mergesort(int* A, int* B, int l, int r,int n,bool steps)
 {
     if(l<r){
-        int m=(l+r)/2; //divide
-        mergesort(A,B,l,m,n,steps);
-        mergesort(A,B,m+1,r,n,steps);
-        merge(A,B,l,m,r);
+        int m=(l+r)/2; //divide the array,m is the middle
+        mergesort(A,B,l,m,n,steps); //call recursively from first to middle
+        mergesort(A,B,m+1,r,n,steps);//call recursively from middle to last
+        int i, j;
+        for (i = m + 1; i > l; i--)
+            B[i - 1] = A[i - 1];
+                for (j = m; j < r; j++)
+                    B[r + m - j] = A[j + 1];
+                        for (int k = l; k <= r; k++)
+                            if (B[j] < B[i])
+                                A[k] = B[j--];
+                                else
+                                A[k] = B[i++];
+
 
         if(steps){
         cout<<"p="<<l<<", r="<<r<<", array=";
@@ -112,6 +85,7 @@ void mergesort(int* A, int* B, int l, int r,int n,bool steps)
         }
         cout<<endl;
         }
+
     }
 
 }
@@ -129,40 +103,38 @@ int radixSort(int* A,int n,int largest,bool steps)
     int divide=1;
     int mod=10;
     int temp;
-
+    // largest is the number of digits
     for (int i=0; i< largest > 0;i++ )
     {
-        int* output=new int[n];
-        int count[10] = {0};
+        int* newArray=new int[n]; //an temp array
+        int digits[10] = {0}; //array to store size of bucket of each digits
         int temp;
         for (int i = 0; i < n; i++)
         {
-            temp=(A[i]/divide)%mod;
-            count[temp]=count[temp]+1;
+            temp=(A[i]/divide)%mod; //divide integer and modulus to get the digits
+            digits[temp]=digits[temp]+1; //count the occurrence of digits in the array
         }
 
         for (int i = 1; i < 10; i++)
         {
-            count[i] =count[i]+count[i - 1];
+            digits[i] =digits[i]+digits[i - 1]; //get the actual position by cumulatively
         }
 
         for (int i = n - 1; i >= 0; i--)
         {
             temp=(A[i]/divide)%mod;
-            output[count[temp]-1] = A[i];
-            count[temp]--;
+            newArray[digits[temp]-1] = A[i];//put the integer in temp array by descending
+            digits[temp]--;  //minus the occurrence so that next position right
         }
 
         for (int i = 0; i < n; i++)
         {
-           A[i] = output[i];
-           /*if(steps)
-            {
-                cout<<A[i]<<" ";
-            }*/
+           A[i] = newArray[i]; //copy back to the original array
         }
-        divide =divide* mod;
-        if(steps){
+
+        divide =divide* mod; //increase the divide number for next digits'loop
+
+        if(steps){ //show steps if enabled
         cout<<"array=";
         for(int i=0;i<n;i++)
         {
@@ -274,7 +246,7 @@ int main()
     system("pause");
     cout<<"Quick Sort: "<<endl;
     TICK();
-    quickSort(A, 0 ,n-1,showSteps);
+    quickSort(A, 0 ,n-1,showSteps, n);
     TOCK();
     for(int i=0;i<n;i++)
         {
@@ -316,7 +288,7 @@ int main()
     {
         A[i]=S[i];
     }
-    cout<<"Merge Sort: ";
+    cout<<"Merge Sort: "<<endl;
     TICK();
 
     //startMergeSort(A,0,n-1,n);
